616eeb8e638c6c474743f70dded9b821276225ff
Max <maximbek9999@gmail.com>
2021-04-07 12:18:54 -0500

    task1

--- taks1/httpd_ec2.tf
+++ taks1/httpd_ec2.tf
@@ -1,8 +1,8 @@
-resource "aws_instance" "apacheec2" {
+resource "aws_instance" "httpd_ec2" {
   availability_zone = "us-west-2b"
   ami               = "ami-0a4497cbe959da512"
   instance_type     = "t2.micro"
-  user_data         = file("./install_apache.sh")
+  user_data         = file("./install_httpd.sh")
   tags = {
     "Name" = "Web-instance"
   }
