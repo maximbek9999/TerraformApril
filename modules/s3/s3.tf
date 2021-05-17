579e5ef0d1475de80e3bde94200cd37d9b6def15
EC2 Default User <ec2-user@ip-172-31-3-193.ec2.internal>
2021-05-17 00:15:59 +0000

    s3

--- modules/s3/s3.tf
+++ modules/s3/s3.tf
@@ -13,3 +13,7 @@ resource "aws_s3_bucket_public_access_block" "bucket_policy" {
   ignore_public_acls = true
   restrict_public_buckets = true
 }
+
+output "s3_name" {
+  value = aws_s3_bucket.main_bucket.bucket_name
+}
